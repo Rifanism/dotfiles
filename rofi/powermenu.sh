52011c66c2278787fd47a6cbae752baa7656cb44
Rif'an Habibi <rifanhabibi.dev@gmail.com>
2026-07-16 23:08:28 +0700

    Update powermenu.sh

--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-# Opsi pilihan menu dengan ikon Nerd Font
 shutdown="  Shutdown"
 reboot="  Reboot"
 sleep="󰤄  Sleep"
 
-# Gabungkan semua opsi
 options="$shutdown\n$reboot\n$sleep"
 
-# Panggil Rofi dengan layout minimalis dan horizontal
 chosen="$(echo -e "$options" | rofi -dmenu \
     -i \
     -p "" \
     -theme-str 'window {width: 350px;} listview {lines: 3;}')"
 
-# Eksekusi perintah berdasarkan pilihan
 case "$chosen" in
     "$shutdown")
         systemctl poweroff
